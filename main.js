26f7c476b1212953fa949e3add5581911f8c14e6
Amy Hong <64371693+itsAmyHong@users.noreply.github.com>
2022-09-19 21:41:12 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,97 @@
+// JavaScript Document
+	function chicken(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/chicken.mp3' hidden='true' autostart='true' loop='false' />";
+		 
+		 var chickenGuess;
+		 
+		 chickenGuess=prompt("What kind of animal am I?");
+		 
+		 if (chickenGuess == "Chicken" || chickenGuess == "chicken"){
+			 alert("Great Job!\nYou are correct, I am a Chicken!");
+		 }
+		 
+		 else{
+		 	alert("Sorry, I am not a " + chickenGuess + ".");
+		 }
+	}
+	
+	function cow(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/cow.mp3' hidden='true' autostart='true' loop='false' />";
+		 
+		 var cowGuess;
+		 
+		 cowGuess=prompt("What kind of animal am I?");
+		 
+		 if (cowGuess == "Cow" || cowGuess == "cow"){
+			 alert("Great Job!\nYou are correct, I am a Cow!");
+		 }
+		 
+		 else{
+		 	alert("Sorry, I am not a " + cowGuess + ".");
+		 }
+	}
+	
+	function goat(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/goat.mp3' hidden='true' autostart='true' loop='false' />";
+		 
+		 var goatGuess;
+		 
+		 goatGuess=prompt("What kind of animal am I?");
+		 
+		 if (goatGuess == "Goat" || goatGuess == "goat"){
+			 alert("Great Job!\nYou are correct, I am a Goat!");
+		 }
+		 
+		 else{
+		 	alert("Sorry, I am not a " + goatGuess + ".");
+		 }
+	}
+	
+	function lamb(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/lamb.mp3' hidden='true' autostart='true' loop='false' />";
+		 
+		 var lambGuess;
+		 
+		 lambGuess=prompt("What kind of animal am I?");
+		 
+		 if (lambGuess == "Lamb" || lambGuess == "lamb"){
+			 alert("Great Job!\nYou are correct, I am a Lamb!");
+		 }
+		 
+		 else{
+		 	alert("Sorry, I am not a " + lambGuess + ".");
+		 }
+	}
+	
+	function pig(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/pig.mp3' hidden='true' autostart='true' loop='false' />";
+		 
+		 var pigGuess;
+		 
+		 pigGuess=prompt("What kind of animal am I?");
+		 
+		 if (pigGuess == "Pig" || pigGuess == "pig"){
+			 alert("Great Job!\nYou are correct, I am a Pig!");
+		 }
+		 
+		 else{
+		 	alert("Sorry, I am not a " + pigGuess + ".");
+		 }
+	}
+	
+	function turkey(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/turkey.mp3' hidden='true' autostart='true' loop='false' />";
+		 
+		 var turkeyGuess;
+		 
+		 turkeyGuess=prompt("What kind of animal am I?");
+		 
+		 if (turkeyGuess == "Turkey" || turkeyGuess == "turkey"){
+			 alert("Great Job!\nYou are correct, I am a Turkey!");
+		 }
+		 
+		 else{
+		 	alert("Sorry, I am not a " + turkeyGuess + ".");
+		 }
+	}
+	
